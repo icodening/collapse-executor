e8ca2eea89927cb514a969b1f84aea77e0af7945
icodening <cleverbn96@gmail.com>
2023-06-06 00:37:16 +0800

    add mark interface

---
+++ collapse-executor-integration/collapse-executor-spring-boot/src/main/java/cn/icodening/collapse/spring/boot/ComponentScanMark.java
@@ -0,0 +1,10 @@
+package cn.icodening.collapse.spring.boot;
+
+/**
+ * Mark the package that needs to be scanned
+ *
+ * @author icodening
+ * @date 2023.06.06
+ */
+public interface ComponentScanMark {
+}
--- collapse-executor-integration/collapse-executor-spring-boot/src/main/java/cn/icodening/collapse/spring/boot/configuration/CollapseComponentScanner.java
+++ collapse-executor-integration/collapse-executor-spring-boot/src/main/java/cn/icodening/collapse/spring/boot/configuration/CollapseComponentScanner.java
@@ -1,5 +1,6 @@
 package cn.icodening.collapse.spring.boot.configuration;
 
+import cn.icodening.collapse.spring.boot.ComponentScanMark;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -7,7 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
  * @author icodening
  * @date 2023.05.24
  */
-@ConfigurationPropertiesScan("cn.icodening.collapse.spring.boot")
-@ComponentScan("cn.icodening.collapse.spring.boot")
+@ConfigurationPropertiesScan(basePackageClasses = ComponentScanMark.class)
+@ComponentScan(basePackageClasses = ComponentScanMark.class)
 class CollapseComponentScanner {
 }
